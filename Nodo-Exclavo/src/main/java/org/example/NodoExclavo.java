a022d31b435e56045cbaf1caece0148182dc3685
GabrielBarrientos99 <129352498+GabrielBarrientos99@users.noreply.github.com>
2023-11-21 11:34:20 -0500

    Add files via upload

---
+++ Nodo-Exclavo/src/main/java/org/example/NodoExclavo.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class NodoExclavo {
+
+    public static void main(String[] args) throws IOException {
+
+        // Crear el archivo cuentas.txt si no existe
+        crearArchivoCuentas();
+
+        Listener e = new Listener(50000);
+        Thread listener = new Thread(e);
+        listener.start();
+
+    }
+
+    private static void crearArchivoCuentas() throws IOException {
+        Path archivoCuentas = Path.of("cuentas.txt");
+
+        // Verificar si el archivo no existe
+        if (Files.notExists(archivoCuentas)) {
+            try {
+                // Crear el archivo
+                Files.createFile(archivoCuentas);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw e; // Relanzar la excepción para manejarla en el método principal
+            }
+        }
+    }
+
+}
